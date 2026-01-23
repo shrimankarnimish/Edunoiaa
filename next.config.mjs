cb73585529d2b44b330fab0775287968f2a60191
Nimish Shrimankar <shrimankarns@gmail.com>
2026-01-23 16:11:33 +0530

    final changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
